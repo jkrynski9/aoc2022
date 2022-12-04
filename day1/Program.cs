b491564e7b0f6d548e4502c5e7294ae8086779d6
jkrynski9 <rafix999@protonmail.ch>
2022-12-04 14:24:35 +0100

    Initial commit, both parts of day 1 and 2 done

---
+++ day1/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] input = new StreamReader("input").ReadToEnd().Split("\n");
+List<int> elves = new List<int>();
+int calories=0;
+for(int i=0;i<input.Length;i++) {
+	if(input[i] == "") {
+		elves.Add(calories);
+		calories=0;
+	}
+	else calories+=int.Parse(input[i]);
+}
+Console.WriteLine(elves.Max());
+elves.Sort();
+elves.Reverse();
+Console.WriteLine(elves[0]+elves[1]+elves[2]);
---
+++ day2/Program.cs
@@ -0,0 +1,73 @@
+﻿string[] input = new StreamReader("input").ReadToEnd().Split("\n");
+const int win=6,draw=3,lose=0;
+int score=0;
+Dictionary<string, string> draws = new Dictionary<string,string>() {{"A","X"},{"B","Y"},{"C","Z"}};
+Dictionary<string, string> rules = new Dictionary<string, string>() {{"A","Y"},{"B","Z"},{"C","X"}};
+foreach(string line in input)
+{
+	string[] values = line.Split(" ");
+	switch(values[1])
+	{
+		case "X":
+		score+=1;
+		break;
+		case "Y":
+		score+=2;
+		break;
+		case "Z":
+		score+=3;
+		break;
+	}
+	foreach(KeyValuePair<string, string> check in rules)
+	{
+		if(values[0]==check.Key)
+		{
+			if(check.Value==values[1]) score+=win;
+			else if(check.Value!=values[0]) score+=lose;
+		}
+	}
+	foreach(KeyValuePair<string, string> check in draws) if(values[0]==check.Key && values[1]==check.Value) score+=draw;
+}
+Console.WriteLine($"Part one: {score}");
+//part two
+score=0;
+Dictionary<string, string> winningPair = new Dictionary<string, string>() {{"A","B"},{"B","C"},{"C","A"}};
+foreach(string line in input)
+{
+	string[] values = line.Split(" ");
+	string choice = "";
+	switch(values[1])
+	{
+	case "X":
+		foreach(KeyValuePair<string, string> check in winningPair)
+			if(values[0]==check.Value) {
+				choice=check.Key;
+				score+=lose;
+			}
+		break;
+	case "Y":
+		choice=values[0];
+		score+=draw;
+		break;
+	case "Z":
+		foreach(KeyValuePair<string,string> check in winningPair)
+			if(values[0]==check.Key) {
+				choice=check.Value;
+				score+=win;
+		}
+		break;
+	}
+	switch(choice)
+	{
+		case "A":
+			score+=1;
+			break;
+		case "B":
+			score+=2;
+			break;
+		case "C":
+			score+=3;
+			break;
+	}
+}
+Console.WriteLine($"Part two: {score}");
